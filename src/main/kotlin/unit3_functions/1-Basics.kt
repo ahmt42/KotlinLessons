45bdec5c67837c604703655d8ba6e69c306745f4
Ahmet Güleç <ahmtgulec@gmail.com>
2022-11-04 23:04:29 +0300

    functions

--- src/main/kotlin/unit3_functions/1-Basics.kt
+++ src/main/kotlin/unit3_functions/1-Basics.kt
@@ -1,2 +1,13 @@
 package unit3_functions
 
+/**
+ *      fonksiyonlar fun keyword'u ile baslar. fonksiyon ismi verilir.
+ *      parametre parantezleri acilir ve parametreler girilir. : operatoru ve geri donus degeri yazilir. fonksiyon body'si acilir ve kapatilir.
+ *      geri donus degeri verilmeyen fonksiyonlar Unit tipini geri dondurur.
+ * **/
+
+//val name: String = "Ahmet"
+//fun name : String = "Ahmet"
+//fun name(val surName : String = "Ahmet") : String = "Ahmet"
+//fun name(surName : String = "Ahmet") : String = "Ahmet"
+
