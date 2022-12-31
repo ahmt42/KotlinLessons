cd686c5d8ddf74a4759b802203d7676d47d902c6
Ahmet Güleç <ahmtgulec@gmail.com>
2022-12-31 21:29:50 +0300

    fold

--- src/main/kotlin/kotlin_collections/2.fold.kt
+++ src/main/kotlin/kotlin_collections/2.fold.kt
@@ -7,7 +7,7 @@ package kotlin_collections
  */
 
 fun main () {
-    //Bir dizideki çift ve tek sayıları gruplandırın ve her gruptaki sayıların toplamını hesaplayın.
+    //Bir dizideki çift ve tek sayıları gruplandırın ve her gruptaki sayıların toplamını hesaplayın :
 
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
     val groupedNumbers = numbers.groupingBy { it % 2 == 0 }.fold(0) { acc, i -> acc + i }
@@ -15,5 +15,15 @@ fun main () {
 
     // groupedNumbers değişkeni şu şekilde bir map olacaktır:
     // {false=36, true=30}
+
+
+    // İlk harfine göre gruplandır ve Harf sayısı çift alan öğeleri al :
+
+    val cities = listOf("Ankara", "Adana", "İstanbul", "İzmir", "Konya", "Kayseri", "Kütahya", "Bursa", "Denizli")
+
+    val evenCities = cities.groupingBy { it.first() }
+        .fold(listOf<String>()) { acc, e -> if (e.length % 2 == 0) acc + e else acc }
+
+    println(evenCities)
 }
 
