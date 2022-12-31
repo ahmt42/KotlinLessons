59ce241f7560974451ab5afdf6517b8368304769
Ahmet Güleç <ahmtgulec@gmail.com>
2022-12-31 21:22:47 +0300

    eachCount

--- src/main/kotlin/kotlin_collections/1.eachCount.kt
+++ src/main/kotlin/kotlin_collections/1.eachCount.kt
@@ -12,10 +12,6 @@ fun main() {
     val groupByCount = list.groupingBy { it }.eachCount()
     println(groupByCount)
 
-    // groupByCount değişkeni şu şekilde bir map olacaktır:
-    // {1: 2, 2: 2, 3: 2, 4: 2, 5: 2}
-
-
     // Bir metin dizisinde kelime sayısını hesaplayın. Her kelimeyi tekrar edenleri ayırarak kelime sayısını hesaplayın.
 
     val words = "Başarılı bir mühendis olarak, yaptığım projelerde en önemli şeyin kalite ve doğruluk olduğunu düşünüyorum, bu yüzden her zaman en iyi malzemeleri ve en yeni teknolojik araçları kullanmaya çalışıyorum, ancak aynı zamanda tasarım esnekliği ve maliyet etkinliğini de dikkate alarak, müşteri beklentilerini karşılamaya çalışıyorum."
