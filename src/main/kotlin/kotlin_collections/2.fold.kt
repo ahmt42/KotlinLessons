22e8f8e05f1b5a2ae93eaff6eeff329f03a8784c
Ahmet Güleç <ahmtgulec@gmail.com>
2022-12-31 21:22:27 +0300

    fold

---
+++ src/main/kotlin/kotlin_collections/2.fold.kt
@@ -0,0 +1,19 @@
+package kotlin_collections
+
+/**
+ * "fold" işleminin amacı, bir gruplama kaynağındaki öğeleri anahtarına göre gruplandırmak ve her grup öğesi için işlemi sırasıyla uygulamaktır.
+ * Bu işlem, önceki birikmiş değer ve mevcut öğeyi argüman olarak alır ve sonuçları yeni bir harita içinde saklar.
+ * Biriktirici için başlangıç değeri, her grup için aynı başlangıç değeridir.
+ */
+
+fun main () {
+    //Bir dizideki çift ve tek sayıları gruplandırın ve her gruptaki sayıların toplamını hesaplayın.
+
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    val groupedNumbers = numbers.groupingBy { it % 2 == 0 }.fold(0) { acc, i -> acc + i }
+    println(groupedNumbers)
+
+    // groupedNumbers değişkeni şu şekilde bir map olacaktır:
+    // {false=36, true=30}
+}
+
