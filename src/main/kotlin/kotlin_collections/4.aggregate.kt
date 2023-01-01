52366dd97fbdd3115489266a7f936e1f02f0037c
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-01 12:20:55 +0300

    aggregate

--- src/main/kotlin/kotlin_collections/4.aggregate.kt
+++ src/main/kotlin/kotlin_collections/4.aggregate.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 
 /**
- * Kotlin dilinde "aggregate" fonksiyonu, bir veri yapısındaki elemanları anahtarına göre gruplamak ve her gruptaki elemanlar üzerinde ardışık olarak bir işlem gerçekleştirmek için kullanılır.
+ * "aggregate" fonksiyonu, bir veri yapısındaki elemanları anahtarına göre gruplamak ve her gruptaki elemanlar üzerinde ardışık olarak bir işlem gerçekleştirmek için kullanılır.
  * Bu işlem, bir lambda ifadesi olarak verilir ve lambda ifadesinin ilk iki argümanı, veri yapısındaki iki elemandır.
  * Bu lambda ifadesinin döndürdüğü değer, bir sonraki eleman ile işleme katılır ve bu şekilde sürekli devam eder.
  * Sonuç olarak, her gruptaki elemanlar üzerinde gerçekleştirilen işlemlerin sonuçlarını içeren yeni bir harita döndürür.
@@ -24,4 +24,15 @@ fun main() {
     }
 
     println(aggregated.values) // [0:3-6-9, 1:4-7, 2:5-8]
+
+    /*
+    Bu kod, 0 ve 99 arasında (dahil) 10 tane rastgele tamsayı üretir ve sonra liste elemanlarını 3'e bölümünden kalan ile gruplar.
+    groupingBy fonksiyonu, kalanların anahtarlar olduğu ve o kalanların elemanların listesi olan bir harita döndürür.
+
+    Sonra, aggregate fonksiyonu kullanılarak, anahtarların kalanlar ve değerlerin liste elemanlarını temsil eden dizgiler olan yeni bir map oluşturulur.
+    aggregate fonksiyonu, liste elemanlarını bir şekilde birleştiren bir lambda fonksiyonu alır.
+    Bu durumda, lambda fonksiyonu, her gruptaki ilk eleman için yeni bir StringBuilder oluşturur ve sonraki elemanlarını ona - ile ayrılmış şekilde ekler.
+
+    Son olarak, aggregated map'inin values özelliği yazdırılır, böylece eleman gruplarını temsil eden dizgi listesi elde edilir.
+     */
 }
