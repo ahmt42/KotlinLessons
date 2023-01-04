2ffdd3d6369e51ccc3b183046acc9d9acc53a6a9
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-05 00:21:07 +0300

    groupingBy

--- src/main/kotlin/kotlin_collections/7.groupingBy.kt
+++ src/main/kotlin/kotlin_collections/7.groupingBy.kt
@@ -1,12 +1,29 @@
 package kotlin_collections
 
-
-data class Person(val name: String, val age: Int)
-
 fun main() {
+    data class Person(val name: String, val age: Int)
+
     val people = listOf(Person("Alice", 25), Person("Bob", 31), Person("Charlie", 31))
 
     val peopleByAge = people.groupingBy { it.age }
     val peopleByAge2 = people.groupBy { it.age }
     println(peopleByAge2)  // Output: {25=[Person(name=Alice, age=25)], 31=[Person(name=Bob, age=31), Person(name=Charlie, age=31)]}
+
+    //
+
+    data class Task(val name: String, val completed: Boolean)
+
+    val tasks = listOf(
+        Task("Take out the trash", true),
+        Task("Do homework", false),
+        Task("Watch TV", true),
+        Task("Go to the gym", false)
+    )
+
+    val taskByStatusCount = tasks.groupingBy { it.completed }.eachCount()
+    val taskByStatus = tasks.groupBy ({ it.completed }, { it.name })
+
+    println(taskByStatus)
+    println(taskByStatusCount)
+
 }
