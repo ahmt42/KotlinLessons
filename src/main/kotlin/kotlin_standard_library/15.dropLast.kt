b7109ffced957e6d5bf4d5cd869be05415f1e3d7
Ahmet Güleç <ahmtgulec@gmail.com>
2023-03-09 00:19:08 +0300

    dropLast

---
+++ src/main/kotlin/kotlin_standard_library/15.dropLast.kt
@@ -0,0 +1,21 @@
+/*
+    dropLast fonksiyonu, Kotlin'de bir koleksiyondan belirli sayıda son elemanı atmak için kullanılır.
+    Bu fonksiyon, bir sayısal değer alır ve koleksiyonun sonundan belirtilen sayıda elemanı atarak yeni bir koleksiyon oluşturur.
+
+    dropLast fonksiyonu aşağıdaki şekilde kullanılabilir:
+ */
+
+fun main() {
+    val chars = ('a'..'z').toList()
+
+    // Drop the last 3 chars
+    val withoutLastThreeChars = chars.dropLast(3)
+
+    // Drop the last 10 chars (if the collection has less than 10 elements, returns an empty collection)
+    val withoutLastTenChars = chars.dropLast(10)
+
+    println("Drop last 3 chars : $withoutLastThreeChars")
+    println("Drop last 10 chars : $withoutLastTenChars")
+}
+
+//
