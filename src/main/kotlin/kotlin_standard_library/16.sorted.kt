15bb7b5ebf0bf5db1818a863ba767547471a7b85
Ahmet Güleç <ahmtgulec@gmail.com>
2023-03-11 23:39:34 +0300

    sorted, sortedDescending

---
+++ src/main/kotlin/kotlin_standard_library/16.sorted.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    val unsortedList = listOf("orange", "apple", "banana", "kiwi")
+    val sortedList = unsortedList.sorted()
+    val sortedDescendingList = unsortedList.sortedDescending()
+
+    println(sortedList) // [apple, banana, kiwi, orange]
+    println(sortedDescendingList ) // [orange, kiwi, banana, apple]
+}
